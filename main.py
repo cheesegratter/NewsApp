39f5769594511e12e5a479a4834faead4085351d
cheesegratter <132412832+cheesegratter@users.noreply.github.com>
2025-05-27 23:14:47 -0400

    new stuff

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ with open("dailyData.json", "r") as file:
 
 dailySummary = []
 for i in range(10):
-    dailySummary.append(dict(topNewsToday['top_news'][i])['news'][0]['title'])
+    dailySummary.append(dict(topNewsToday['top_news'][i])['news'][0]['id'])
 
 print(dailySummary)
