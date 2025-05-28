dcc1fa25c52a3d0a109a94aab55611a266b5ec0d
cheesegratter <132412832+cheesegratter@users.noreply.github.com>
2025-05-28 01:39:28 -0400

    more setup

--- main.py
+++ main.py
@@ -4,8 +4,13 @@ import json
 with open("dailyData.json", "r") as file:
     topNewsToday = json.load(file)
 
-dailySummary = []
-for i in range(10):
-    dailySummary.append(dict(topNewsToday['top_news'][i])['news'][0]['id'])
+dailyDate = dict(topNewsToday['top_news'][0])['news'][0]['publish_date'][0:10]
 
-print(dailySummary)
+dailyRow = ""
+
+for i in range(10):
+    dailyRow = dailyRow + str(dict(topNewsToday['top_news'][i])['news'][0]['id']) + "," + str(dict(topNewsToday['top_news'][i])['news'][0]['title']) + "," + str(dict(topNewsToday['top_news'][i])['news'][0]['text'])
+
+
+with open("dailyNews.csv", "a") as f:
+  f.write("\n" + dailyDate + "," +  dailyRow[:-1])
