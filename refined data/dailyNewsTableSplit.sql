1ee3e88578e4fbcfd40c8e161258e7bbcb2880d5
cheesegratter <132412832+cheesegratter@users.noreply.github.com>
2025-06-24 15:31:51 -0400

    database linking!

--- refined data/dailyNewsTableSplit.sql
+++ refined data/dailyNewsTableSplit.sql
@@ -1,59 +1,59 @@
 DROP TABLE ARTICLE1;
 
 CREATE TABLE ARTICLE1 AS
-    SELECT DATE, newsID1, newsTitle1
+    SELECT DATE, newsID1, newsTitle1 as newsTitle
     FROM dailyNewsClean;
 
 DROP TABLE ARTICLE2;
 
 CREATE TABLE ARTICLE2 AS
-    SELECT DATE, newsID2, newsTitle2
+    SELECT DATE, newsID2, newsTitle2 as newsTitle
     FROM dailyNewsClean;
 
 DROP TABLE ARTICLE3;
 
 CREATE TABLE ARTICLE3 AS
-    SELECT DATE, newsID3, newsTitle3
+    SELECT DATE, newsID3, newsTitle3 as newsTitle
     FROM dailyNewsClean;
 
 DROP TABLE ARTICLE4;
 
 CREATE TABLE ARTICLE4 AS
-    SELECT DATE, newsID4, newsTitle4
+    SELECT DATE, newsID4, newsTitle4 as newsTitle
     FROM dailyNewsClean;
 
 DROP TABLE ARTICLE5;
 
 CREATE TABLE ARTICLE5 AS
-    SELECT DATE, newsID5, newsTitle5
+    SELECT DATE, newsID5, newsTitle5 as newsTitle
     FROM dailyNewsClean;
 
 DROP TABLE ARTICLE6;
 
 CREATE TABLE ARTICLE6 AS
-    SELECT DATE, newsID6, newsTitle6
+    SELECT DATE, newsID6, newsTitle6 as newsTitle
     FROM dailyNewsClean;
 
 DROP TABLE ARTICLE7;
 
 CREATE TABLE ARTICLE7 AS
-    SELECT DATE, newsID7, newsTitle7
+    SELECT DATE, newsID7, newsTitle7 as newsTitle
     FROM dailyNewsClean;
 
 DROP TABLE ARTICLE8;
 
 CREATE TABLE ARTICLE8 AS
-    SELECT DATE, newsID8, newsTitle8
+    SELECT DATE, newsID8, newsTitle8 as newsTitle
     FROM dailyNewsClean;
 
 DROP TABLE ARTICLE9;
 
 CREATE TABLE ARTICLE9 AS
-    SELECT DATE, newsID9, newsTitle9
+    SELECT DATE, newsID9, newsTitle9 as newsTitle
     FROM dailyNewsClean;
 
 DROP TABLE ARTICLE10;
 
 CREATE TABLE ARTICLE10 AS
-    SELECT DATE, newsID10, newsTitle10
+    SELECT DATE, newsID10, newsTitle10 as newsTitle
     FROM dailyNewsClean;
